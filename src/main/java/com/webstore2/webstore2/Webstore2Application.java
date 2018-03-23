aefe3ec96a39079535a2df5ce4a272ba359621c9
Yurii Arabchuk <yura.arab4uk@gmail.com>
2018-03-23 15:03:56 +0200

    Initial commit

---
+++ src/main/java/com/webstore2/webstore2/Webstore2Application.java
@@ -0,0 +1,12 @@
+package com.webstore2.webstore2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Webstore2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Webstore2Application.class, args);
+	}
+}
